2a6141aa966ab8ba9916cccc38d3a7a41f219e92
ArturT <artur.trzop@lunarlogic.io>
2015-09-09 17:54:02 +0200

    task 2: handle default name

--- tasks/2/script.js
+++ tasks/2/script.js
@@ -4,11 +4,13 @@
 var helloWorld = React.createClass({
   render: function() {
     return (<div>
-            Hellow {this.props.name}!
+            Hellow {this.props.name ? this.props.name : 'World'}!
             </div>);
   }
 });
 
 var helloWorldElement = React.createElement(helloWorld, { name: 'Artur' })
+var helloWorldElement2 = React.createElement(helloWorld)
 
 React.render(helloWorldElement, document.getElementById('main'));
+React.render(helloWorldElement2, document.getElementById('main2'));
