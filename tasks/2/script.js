8ec9da8f30e89c7162ba8f60fa4e206b979dd15e
ArturT <artur.trzop@lunarlogic.io>
2015-09-09 18:30:47 +0200

    task 2: render names [wip]

--- tasks/2/script.js
+++ tasks/2/script.js
@@ -14,3 +14,22 @@ var helloDefault = React.createElement(Hello)
 
 React.render(hello, document.getElementById('name'));
 React.render(helloDefault, document.getElementById('default-name'));
+
+
+var Helloes = React.createClass({
+  render: function() {
+    var names = this.props.names.map(function(name) {
+      React.createElement(Hello, { name: name })
+    })
+
+    return (
+      <div>
+      {names}
+      </div>
+    );
+  }
+})
+
+var helloes = React.createElement(Helloes, { names: ['Adam', 'Ewa'] })
+
+React.render(helloes, document.getElementById('names'))
