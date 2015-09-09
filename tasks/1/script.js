363d7cffecde9e53d370e1306b0f55100db55ba3
ArturT <artur.trzop@lunarlogic.io>
2015-09-09 16:02:43 +0200

    solve task 1, render hello world

--- tasks/1/script.js
+++ tasks/1/script.js
@@ -1,3 +1,14 @@
 /*
-    Exercise 1 
+    Exercise 1
 */
+var helloWorld = React.createClass({
+  render: function() {
+    return (<div>
+            Hellow World!
+            </div>);
+  }
+});
+
+var helloWorldElement = React.createElement(helloWorld)
+
+React.render(helloWorldElement, document.getElementById('main'));
