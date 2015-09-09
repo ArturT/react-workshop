f4aa99d67397b2ee6e18fe5b28d3f54253a92bd1
ArturT <artur.trzop@lunarlogic.io>
2015-09-09 17:17:26 +0200

    task 2, hello name (step 1)

--- tasks/2/script.js
+++ tasks/2/script.js
@@ -1,3 +1,14 @@
 /*
     Exercise 2
 */
+var helloWorld = React.createClass({
+  render: function() {
+    return (<div>
+            Hellow {this.props.name}!
+            </div>);
+  }
+});
+
+var helloWorldElement = React.createElement(helloWorld, { name: 'Artur' })
+
+React.render(helloWorldElement, document.getElementById('main'));
